d60c460caf55331c281e6ed2fcb6399b7a8489cc
I T_code <oluwagbotemioyeboade@gmail.com>
2023-01-14 22:40:26 +0100

    Constant variable.js

---
+++ Constant variable.js
@@ -0,0 +1,8 @@
+const a = 'Hello';
+a = 'Bye';
+console.log(a);
+
+/*this code return error due to 
+the re assigning of the a variable 
+that is already said to be a constant variable*/
+/*const variable are immutable*/
