19feac51922eb718ef5d41f785f3a584276872ed
I T_code <oluwagbotemioyeboade@gmail.com>
2023-01-14 23:00:38 +0100

    ES6 escape literal.js

---
+++ ES6 Escape literal.js
@@ -0,0 +1,8 @@
+let a = 8;
+let b = 34;
+let msg = `The sum\`s is ${a+b}`;
+console.log(msg);
+/*The backslash (\) is used to escape string
+from back tick,simgle quote or double quote,
+it makes javascript treat the backtick encounter after backlash as part of string to be printed byt
+not as the quote wrapping the string*/
