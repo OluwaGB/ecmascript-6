5250b654a7f358eea707c600e8854abf603cb1bc
I T_code <oluwagbotemioyeboade@gmail.com>
2023-01-14 23:08:14 +0100

    Create Array syntax.js
    The below is the array syntax prior to ecma script six array syntax

---
+++ Array syntax.js
@@ -0,0 +1,10 @@
+let arr = [1, 2, 3];
+for (let k = 0; k < arr.length; k++) {
+	console.log(arr[k]);
+}/*Es6 updated array syntax*/
+
+/* js array syntax*/
+for (x = 0; x<10; x++){ x+=2;
+console.log(x);
+	
+}
