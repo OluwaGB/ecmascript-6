bc704c9ce42009c9b4512b7a2c661955bde1ef4f
I T_code <oluwagbotemioyeboade@gmail.com>
2023-01-16 23:49:20 +0100

    Create ES6 Loop statement.js
    This a for in loop statement use for iterating array list

---
+++ ES6 Loop statement.js
@@ -0,0 +1,17 @@
+let list = ["x", "y", "z"];
+for (let val of list) {
+	console.log(val);
+}
+/* another type of loop statement in an ES6 is tge for of loop statement with the syntax:
+Let array = ["list","list","list"];
+for (let x of array){
+	console.log(val);
+}
+*/
+let array =["iyanuoluwa","peter",23,"male"];
+for (let x of array){
+	console.log(x);
+}
+/*unlike the for in loop statement,the for of loop statement is used to print the value of the list of an array*/
+array = [60,45,76,70,54,77,56,60,78]
+if(pass==70){(console.log(pass.length)}
