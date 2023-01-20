a87dcd25df0154e8054558ed197422e0184b1ec1
I T_code <oluwagbotemioyeboade@gmail.com>
2023-01-20 23:51:26 +0100

    It is the the reassigning of value to array list

---
+++ Array destructuring syntax.js
@@ -0,0 +1,12 @@
+let arr = ['1', 2,4, '3'];
+
+let [one, two, four,three] = arr;//array now variable
+
+console.log(one); // 1
+console.log(two); // 2
+console.log(three); // 3
+console.log(four);
+/* syntax 
+arr = [list,list list] 
+
+new array = arr */
