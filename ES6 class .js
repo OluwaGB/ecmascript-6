f1416dfbad154179c63c484a6e924d3518d143ac
I T_code <oluwagbotemioyeboade@gmail.com>
2023-02-01 22:47:33 +0100

    Create ES6 class .js

---
+++ ES6 class .js
@@ -0,0 +1,21 @@
+class Rectangle {
+	constructor(height, width) {
+		this.height = height;
+		this.width = width;
+  }
+}
+
+const square = new Rectangle(5, 5);
+const poster = new Rectangle(2, 3); 
+
+console.log(square.height);
+class profile{
+	constructor(name,age){
+		this.name = name;
+		this.age = age;
+	}
+}
+const iyanu = new profile("iyanuoluwa",22);
+const peter = new profile("peter",23);
+console.log(iyanu.name);
+console.log(peter.age);
