cd5c075eeb63427f15f888bf0e3427f6917788f9
I T_code <oluwagbotemioyeboade@gmail.com>
2023-03-16 01:39:41 +0100

    Several ways of getting random numbers in Js
    The above snippet consist of method by which you can get random numbers (integers and float )in JavaScript

---
+++ Random numbers.js
@@ -0,0 +1,5 @@
+Math.floor(Math.random() * 100)
+//Output: random numbers between 0 and 99
+Math.floor(Math.random() * 100) + 1;
+//Output will be random numbers between 1 and 99
+Math.random()//output will be random float numbers less than zero
