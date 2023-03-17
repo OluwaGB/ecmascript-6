0fd6e82abd9eca6449b80b6a035a5bad82ff7803
I T_code <oluwagbotemioyeboade@gmail.com>
2023-03-18 00:54:16 +0100

    Create Indexing.js
    Method of accessing the index of a variable

---
+++ Indexing.js
@@ -0,0 +1,4 @@
+Let name = "Iyanuoluwa"
+lastLetterOfName = name[name -1]//method of acessing the last index of variable name values
+name[9]//this is another way of acessing the last 
+letter of the values of the variable name
